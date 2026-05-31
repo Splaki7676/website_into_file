1f3783e46478a68a05d6988850bb99a6aa8dbe12
Arad Levi <aradlevi13@gmail.com>
2026-05-31 09:58:03 +0300

    add an photo (not done!!) file

---
+++ site_photos.py
@@ -0,0 +1,89 @@
+PAGES_IMAGES = {
+    # =========================
+    # Admin pages
+    # =========================
+    "AdminAddUser.aspx": [
+        "https://YOUR_URL/AdminAddUser1.png",
+        "https://YOUR_URL/AdminAddUser2.png",
+    ],
+    "AdminDashboard.aspx": ["https://YOUR_URL/AdminDashboard.png"],
+    "AdminEditUser.aspx": ["https://YOUR_URL/AdminEditUser.png"],
+    "AdminUsers.aspx": ["https://YOUR_URL/AdminUsers.png"],
+    # =========================
+    # Main system pages
+    # =========================
+    "HomePage.aspx": [
+        "https://i.imgur.com/WE1Xw25.png",
+        "https://i.imgur.com/x0M9JJW.png",
+        ###########
+        # complet
+        ###########
+    ],
+    "LoginPage.aspx": [
+        "https://i.imgur.com/MeVVdpk.png"
+        ###########
+        # complet
+        ###########
+    ],
+    "RegisterPage.aspx": [
+        "https://i.imgur.com/IwQCDr6.png",
+        "https://i.imgur.com/gbWy9JF.png",
+        ###########
+        # complet
+        ###########
+    ],
+    "ForgotPassword.aspx": [
+        "https://i.imgur.com/U1k2YUr.png"
+        ###########
+        # complet
+        ###########
+    ],
+    "ResetPassword.aspx": ["https://YOUR_URL/ResetPassword.png"],
+    "myAccount.aspx": ["https://YOUR_URL/MyAccount.png"],
+    "EditProfile.aspx": ["https://YOUR_URL/EditProfile.png"],
+    "Calculator.aspx": ["https://YOUR_URL/Calculator.png"],
+    "TypesPage.aspx": [
+        "https://i.imgur.com/tZWFggY.png",
+        "https://i.imgur.com/DEiREMH.png",
+        "https://i.imgur.com/I1uP2Hv.png",
+        "https://i.imgur.com/u0Ppu3D.jpeg",
+        "https://i.imgur.com/te2fpc7.jpeg",
+        "https://i.imgur.com/fAjR5Db.png",
+        ###########
+        # complet
+        ###########
+    ],
+    "Sounds.aspx": [
+        "https://i.imgur.com/pHqZr6i.png"
+        ###########
+        # complet
+        ###########
+    ],
+    "Guess_the_Sound.aspx": [
+        "https://i.imgur.com/8paZPrS.png"
+        ###########
+        # complet
+        ###########
+    ],
+    "GameResults.aspx": [
+        "https://i.imgur.com/y1sn5wg.png"
+        ###########
+        # complet
+        ###########
+    ],
+    # =========================
+    # knoleg_master(nested)
+    # =========================
+    "BondingWithHorse.aspx": ["https://YOUR_URL/Bonding.png"],
+    "FeedingByAge.aspx": ["https://YOUR_URL/Feeding.png"],
+    "ForbiddenFoods.aspx": ["https://YOUR_URL/ForbiddenFoods.png"],
+    "GrainsAndFeed.aspx": ["https://YOUR_URL/Grains.png"],
+    "HayAndGrass.aspx": ["https://YOUR_URL/HayGrass.png"],
+    "HorseCare.aspx": ["https://YOUR_URL/HorseCare.png"],
+    "HorseHealth.aspx": ["https://YOUR_URL/HorseHealth.png"],
+    "HorseNutritionOverview.aspx": ["https://YOUR_URL/Nutrition.png"],
+    "RidingPage.aspx": ["https://YOUR_URL/Riding.png"],
+    "SeasonalFeeding.aspx": ["https://YOUR_URL/Seasonal.png"],
+    "Supplements.aspx": ["https://YOUR_URL/Supplements.png"],
+    "WaterNeeds.aspx": ["https://YOUR_URL/Water.png"],
+}
